24ec60605aa6d0de448138555a57fa013e46e4ee
atsushi421 <atsushi.yano.2@tier4.jp>
2023-10-25 18:36:18 +0900

    feat: skip container
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- auto_isolated_measurement/src/bin/save_dynamic_node_info.rs
+++ auto_isolated_measurement/src/bin/save_dynamic_node_info.rs
@@ -1,9 +1,16 @@
+use auto_isolated_measurement::utils::{create_progress_bar, run_command, NodeNameConverter};
 use std::fs::{self, File};
 use std::io::Write;
-use auto_isolated_measurement::utils::{run_command, create_progress_bar, NodeNameConverter};
 
 const OUTPUT_DIR: &str = "dynamic_node_info";
-const SKIP_NODES: [&str; 5] = ["caret_", "launch_ros_", "rviz2", "rosbag2_player", "transform_listener_impl_"];
+const SKIP_NODES: [&str; 6] = [
+    "caret_",
+    "launch_ros_",
+    "rviz2",
+    "rosbag2_player",
+    "transform_listener_impl_",
+    "container",
+];
 
 pub fn main() {
     let node_list_str = run_command("ros2 node list");
@@ -11,7 +18,10 @@ pub fn main() {
     let pb = create_progress_bar(node_list.clone().count() as i32);
     for ros_node_name in node_list {
         pb.inc(1);
-        if SKIP_NODES.iter().any(|&skip_node| ros_node_name.contains(skip_node)) {
+        if SKIP_NODES
+            .iter()
+            .any(|&skip_node| ros_node_name.contains(skip_node))
+        {
             continue;
         }
 
@@ -20,7 +30,12 @@ pub fn main() {
         let info_text = info_lines[1..].join("\n");
 
         fs::create_dir_all(OUTPUT_DIR).unwrap();
-        let mut file = File::create(format!("{}/{}.yaml", OUTPUT_DIR, NodeNameConverter::to_file_name(ros_node_name))).unwrap();
+        let mut file = File::create(format!(
+            "{}/{}.yaml",
+            OUTPUT_DIR,
+            NodeNameConverter::to_file_name(ros_node_name)
+        ))
+        .unwrap();
         file.write_all(info_text.as_bytes()).unwrap();
     }
 }
