4df00bfb0752a289fd6692f2723f520ba9cd9dfb
atsushi421 <atsushi.yano.2@tier4.jp>
2023-10-27 02:23:09 +0900

    refactor: edge_case.rs
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- auto_isolated_measurement/src/edge_cases.rs
+++ auto_isolated_measurement/src/edge_cases.rs
@@ -5,23 +5,14 @@ use crate::{parse_launch::LaunchParser, utils::search_files};
 pub fn parse_occupancy_grid_map_node(_target_dir: &str) -> (String, String, Vec<(String, String)>) {
     let launch_parser = LaunchParser::new("occupancy_grid_map_node");
     let launch_file = read_to_string("/home/atsushi/autoware/src/universe/autoware.universe/perception/probabilistic_occupancy_grid_map/launch/pointcloud_based_occupancy_grid_map.launch.py").unwrap();
-    let result = &launch_parser.parse_candidate_py(&launch_file)[0];
-
-    (
-        result.package.clone(),
-        result.plugin.as_ref().unwrap().replace('"', "").clone(),
-        result.remappings.as_ref().unwrap().clone(),
-    )
-}
-
-pub fn parse_gyro_odometer(_target_dir: &str) -> (String, String, Vec<(String, String)>) {
-    let launch_parser = LaunchParser::new("gyro_odometer_node");
-    let launch_file = read_to_string("/home/atsushi/autoware/src/universe/autoware.universe/localization/gyro_odometer/launch/gyro_odometer.launch.xml").unwrap();
-    let result = launch_parser.parse_confirmed_xml(&launch_file);
+    let result = launch_parser
+        .parse_candidate_py(&launch_file)
+        .pop()
+        .unwrap();
 
     (
         result.package,
-        result.executable.unwrap(),
+        result.plugin.as_ref().unwrap().replace('"', ""),
         result.remappings.unwrap(),
     )
 }
@@ -36,7 +27,7 @@ pub fn parse_aggregator_node(target_dir: &str) -> (String, String) {
 }
 
 pub fn parse_driver_ros_wrapper_node(_target_dir: &str) -> (String, String, String) {
-    // HACK
+    // HACK: hard coded
     (
         "nebula_ros".to_string(),
         "VelodyneDriverRosWrapper".to_string(),
