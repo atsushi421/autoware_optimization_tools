527b4faacfa22a4c703e1e418777cc855768468d
atsushi421 <atsushi.yano.2@tier4.jp>
2023-10-26 22:30:22 +0900

    refactor: parse_plugin.rs
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- auto_isolated_measurement/src/parse_plugin.rs
+++ auto_isolated_measurement/src/parse_plugin.rs
@@ -6,17 +6,19 @@ pub fn parse_plugin(plugin_name: &str, target_dir: &str, node_name: &str) -> Str
     if plugin_name.starts_with('\"') {
         plugin_name.replace('\"', "")
     } else {
-        // get plugin name from param.yaml
-        let plugin_param_pattern = Regex::new(r#"self\.(\w+)_param\["\w+"\]\["plugin"\]"#).unwrap();
-        let plugin_param = plugin_param_pattern
+        // Get plugin_name from param.yaml
+        let plugin_param_prefix = Regex::new(r#"self\.(\w+)_param\["\w+"\]\["plugin"\]"#)
+            .unwrap()
             .captures(plugin_name)
             .unwrap()
             .get(1)
             .unwrap()
             .as_str();
 
-        let param_yaml = &search_files(target_dir, &format!("{}.param.yaml", plugin_param))[0];
-        let param_map = read_yaml_as_mapping(&param_yaml);
+        let param_yaml =
+            &search_files(target_dir, &format!("{}.param.yaml", plugin_param_prefix))[0];
+
+        let param_map = read_yaml_as_mapping(param_yaml);
         param_map["/**"]["ros__parameters"][node_name]["plugin"]
             .as_str()
             .unwrap()
