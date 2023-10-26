9f8b5377067cf76e25519b3b69b1c0f60a6b4b17
atsushi421 <atsushi.yano.2@tier4.jp>
2023-10-26 15:48:20 +0900

    refactor: use LaunchParser in edge_cases
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- auto_isolated_measurement/src/edge_cases.rs
+++ auto_isolated_measurement/src/edge_cases.rs
@@ -1,31 +1,24 @@
 use std::fs::read_to_string;
 
-use regex::Regex;
-
+use crate::parse_launch::LaunchParser;
 use crate::utils::search_file;
 
 pub fn parse_map_projection_loader(target_dir: &str) -> (String, String) {
     let launch_file =
         read_to_string(search_file(target_dir, "map_projection_loader.launch.xml")).unwrap();
-    let re = Regex::new(r#"pkg="([^"]+)"\s+exec="([^"]+)"#).unwrap();
+    let launch_parser = LaunchParser::new("map_projection_loader");
+    let result = launch_parser.parse_launch_xml(&launch_file);
 
-    let caps = re.captures(&launch_file).unwrap();
-    (
-        caps.get(1).unwrap().as_str().to_string(),
-        caps.get(2).unwrap().as_str().to_string(),
-    )
+    (result.package, result.executable.unwrap())
 }
 
 pub fn parse_topic_state_monitor(target_dir: &str) -> (String, String) {
     let launch_file =
         read_to_string(search_file(target_dir, "topic_state_monitor.launch.xml")).unwrap();
-    let re = Regex::new(r#"pkg="([^"]+)" +exec="([^"]+)"#).unwrap();
+    let launch_parser = LaunchParser::new("dummy");
+    let result = launch_parser.parse_launch_xml(&launch_file);
 
-    let caps = re.captures(&launch_file).unwrap();
-    (
-        caps.get(1).unwrap().as_str().to_string(),
-        caps.get(2).unwrap().as_str().to_string(),
-    )
+    (result.package, result.executable.unwrap())
 }
 
 pub fn parse_driver_ros_wrapper_node(_target_dir: &str) -> (String, String, String) {
