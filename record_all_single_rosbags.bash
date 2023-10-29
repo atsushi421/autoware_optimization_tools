30925bc5b857e1e7e8e0977bb394ad97902adea6
atsushi421 <atsushi.yano.2@tier4.jp>
2023-10-29 11:52:00 +0900

    refactor: improve readability
    Signed-off-by: atsushi421 <atsushi.yano.2@tier4.jp>

--- record_all_single_rosbags.bash
+++ record_all_single_rosbags.bash
@@ -7,29 +7,31 @@ if [ -z "$complete_node_info_dir" ]; then
   exit 1
 fi
 
-for file in "$complete_node_info_dir"/*; do
-  echo "$file"
+WAITING_TIME_FOR_AUTOWARE_TO_LAUNCH=120
+WAITING_TIME_FOR_RECORDER_TO_START=15
+WAITING_TIME_FOR_LOCALIZATION=20
+WAITING_TIME_FOR_SIMULATION_TO_END=160
+GOAL_POSITION_MSG='{header: {frame_id: "map"}, pose: {position: {x: 89516.8, y: 42442.2, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.840247 , w: 0.542204}}}'
 
+for complete_node_info in "$complete_node_info_dir"/*; do
   (setsid ros2 launch autoware_launch logging_simulator.launch.xml map_path:="$HOME/autoware_map/sample-map-rosbag" vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit) &
-  pid1=$!
+  autoware_pid=$!
 
-  sleep 110
+  sleep "$WAITING_TIME_FOR_AUTOWARE_TO_LAUNCH"
 
-  (setsid python3 "$HOME/autoware_optimization_tools/ros2_single_node_replayer/recorder.py" "$file") &
-  pid2=$!
+  (setsid python3 "$HOME/autoware_optimization_tools/ros2_single_node_replayer/recorder.py" "$complete_node_info") &
+  recorder_pid=$!
 
-  sleep 15
+  sleep "$WAITING_TIME_FOR_RECORDER_TO_START"
 
   ros2 bag play "$HOME/autoware_map/sample-rosbag/sample.db3" -r 0.2 -s sqlite3 &
 
-  sleep 20
+  sleep "$WAITING_TIME_FOR_LOCALIZATION"
 
-  ros2 topic pub -1 /planning/mission_planning/goal geometry_msgs/PoseStamped '{header: {frame_id: "map"}, pose: {position: {x: 89516.8, y: 42442.2, z: 0.0}, orientation: {x: 0.0, y: 0.0, z: 0.840247 , w: 0.542204}}}' &
+  ros2 topic pub -1 /planning/mission_planning/goal geometry_msgs/PoseStamped "$GOAL_POSITION_MSG" &
 
-  sleep 160
+  sleep "$WAITING_TIME_FOR_SIMULATION_TO_END"
 
-  kill -- -"$pid2"
-
-  kill -- -"$pid1"
-
+  kill -- -"$recorder_pid"
+  kill -- -"$autoware_pid"
 done
